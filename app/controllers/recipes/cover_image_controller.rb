c0af696e4f3a6cd466006efbb1bfbf15bfbbe481
kamend0 <mendoza.kollin@gmail.com>
2024-02-03 12:22:19 -0600

    turbo for form-preserving deletion

--- app/controllers/recipes/cover_image_controller.rb
+++ app/controllers/recipes/cover_image_controller.rb
@@ -1,10 +1,15 @@
 class Recipes::CoverImageController < ApplicationController
+    include ActionView::RecordIdentifier
+
     before_action :authenticate_user!
     before_action :set_recipe
 
     def destroy
         @recipe.cover_image.purge_later
-        redirect_to edit_recipe_path(@recipe)
+        respond_to do |format|
+            format.html { redirect_to edit_recipe_path(@recipe) }
+            format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@recipe, :cover_image)) }
+        end
     end
 
     private
