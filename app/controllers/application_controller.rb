5b959c8577e1e73a424cc5d585fb561c10236c74
kamend0 <mendoza.kollin@gmail.com>
2024-02-04 16:01:25 -0600

    direct log ins to recipes, but enable home re-visiting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
-end
+    protected
+  
+    def after_sign_in_path_for(resource)
+      # Customize the path based on your requirements
+      if resource.is_a?(User)
+        recipes_path
+      else
+        super  # Default path
+      end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'pages/home'
   devise_for :users
+
   root "pages#home"
 
   resources :recipes do
     resources :ingredients
     resources :cover_image, only: [:destroy], module: :recipes
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
